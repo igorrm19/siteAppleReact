219f1191ce72e9309edbf4a19bd0d4cd44f2ed95
igorrm23 <igormachadofire14@gmail.com>
2026-07-13 18:43:19 -0300

    feat: update header component with navigation icons and hover animations

--- src/components/headerComponent.tsx
+++ src/components/headerComponent.tsx
@@ -1,59 +1,71 @@
+import logoApple from "../assets/logoApple.svg";
+import search from "../assets/search.svg";
+import bag from "../assets/bag.svg";
 import "../styles/pages/homePage.css";
 
 function Header() {
     return (
-        <header className="flex w-screen h-[15vh]">
-            <nav className="flex w-full h-[30px]">
+        <header className="flex w-screen h-[10vh]">
+            <nav className="flex justify-between items-center w-full h-full">
                 <ul className="flex w-full justify-center items-center h-14">
                     <li className="list">
+                        <img src={logoApple} alt="logo" className="cursor-pointer" />
+                    </li>
+                    <li className="list">
                         <a href="#">
-                            <h1 className="text-2xl">Store</h1>
+                            <h1 className="text-2xl cursor-pointer">Store</h1>
                         </a>
                     </li>
                     <li className="list">
                         <a href="#">
-                            <h1 className="text-2xl">Mac</h1>
+                            <h1 className="text-2xl cursor-pointer">Mac</h1>
                         </a>
                     </li>
                     <li className="list">
                         <a href="#">
-                            <h1 className="text-2xl">Ipad</h1>
+                            <h1 className="text-2xl cursor-pointer">Ipad</h1>
                         </a>
                     </li>
                     <li className="list">
                         <a href="#">
-                            <h1 className="text-2xl">Iphone</h1>
+                            <h1 className="text-2xl cursor-pointer">Iphone</h1>
                         </a>
                     </li>
                     <li className="list">
                         <a href="#">
-                            <h1 className="text-2xl">Watch</h1>
+                            <h1 className="text-2xl cursor-pointer">Watch</h1>
                         </a>
                     </li>
                     <li className="list">
                         <a href="#">
-                            <h1 className="text-2xl">Airpods</h1>
+                            <h1 className="text-2xl cursor-pointer">Airpods</h1>
                         </a>
                     </li>
                     <li className="list">
                         <a href="#">
-                            <h1 className="text-2xl">TV & Home</h1>
+                            <h1 className="text-2xl cursor-pointer">TV & Home</h1>
                         </a>
                     </li>
                     <li className="list">
                         <a href="#">
-                            <h1 className="text-2xl">Entertainment</h1>
+                            <h1 className="text-2xl cursor-pointer">Entertainment</h1>
                         </a>
                     </li>
                     <li className="list">
                         <a href="#">
-                            <h1 className="text-2xl">Accessories</h1>
+                            <h1 className="text-2xl cursor-pointer">Accessories</h1>
                         </a>
                     </li>
                     <li className="list">
                         <a href="#">
-                            <h1 className="text-2xl">Support</h1>
+                            <h1 className="text-2xl cursor-pointer">Support</h1>
                         </a>
+                    </li>
+                    <li className="list">
+                        <img src={search} alt="search" className="cursor-pointer" />
+                    </li>
+                    <li className="list">
+                        <img src={bag} alt="bag" className="cursor-pointer" />
                     </li>
                 </ul>
             </nav>
