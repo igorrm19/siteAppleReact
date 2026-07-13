217879c56f1a776d58eac297b7293c2f0e3e500f
igorrm23 <igormachadofire14@gmail.com>
2026-07-13 16:46:04 -0300

    feat: migrate to Tailwind CSS and replace App component with home page structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import HomePage from './pages/homePage.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <StrictMode>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </StrictMode>
+  </BrowserRouter>,
 )
---
+++ src/pages/homePage.tsx
@@ -0,0 +1,12 @@
+import HeaderComponent from "../components/headerComponent.tsx"
+import "../styles/pages/homePage.css"
+
+function HomePage() {
+    return (
+        <div>
+            <HeaderComponent />
+        </div>
+    )
+}
+
+export default HomePage
