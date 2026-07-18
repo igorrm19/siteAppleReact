c20d4f1669ad83794570cd61b66769525c743cca
igorrm23 <igormachadofire14@gmail.com>
2026-07-18 17:48:05 -0300

    feat: add background figure SVG and integrate it into home page

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,6 +1,7 @@
 import HeaderComponent from "../components/headerComponent.tsx"
 import heroImage from "../../public/figure.png"
 import imageFundo from "../../public/div (1).svg"
+import iPhone from "../../public/figure.svg"
 
 function HomePage() {
     return (
@@ -22,9 +23,15 @@ function HomePage() {
                 <section className="w-screen h-screen flex flex-col items-center justify-center">
                     <img src={imageFundo} alt="" className="w-full h-screen" />
                 </section>
-                <section className="w-screen h-screen flex flex-col items-center justify-center bg-white">
-                    <span className="text-2xl font-medium">Iphone 16</span>
-                    <p>O futuro em suas mãos</p>
+                <section className="w-screen h-screen flex flex-col items-center justify-center bg-slate-50">
+                    <div className="flex flex-col items-center justify-center">
+                        <h1 className="font-bold text-6xl" style={{ color: "#1D1D1F" }}>iPhone 14</h1>
+                        <h3 className="font-medium text-3xl" style={{ color: "#1D1D1F" }}>Big and bigger.</h3>
+                        <p className="text-blue-600 font-medium text-2xl cursor-pointer mt-5">Saber mais</p>
+                    </div>
+                    <div>
+                        <img src={iPhone} alt="iPhone 14" />
+                    </div>
                 </section>
                 <section className="w-screen h-screen flex flex-col items-center justify-center">
                     <span className="text-2xl font-medium">Iphone 16</span>
